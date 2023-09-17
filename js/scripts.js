8678b1f4ba8ded2d83017e9442ab103547624b7b
Yemi Idowu <yemsonidowu@gmail.com>
2023-09-17 11:49:16 -0700

    Added pokemonList
    First deleted the previous exercise js, added array of objects, which each object is a pokemon, and each pokemon has different variables (name, height, type, and their national pokedex number).

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-alert("Hello World");
-
-let favoriteFood = "Sushi";
-document.write(favoriteFood);
+let pokemonList = [
+    {name: "abra", height: 0.9, type: "psychic", nationalNum: 63}, 
+    {name: "scyther", height: 1.5, type: ["bug", "flying"], nationalNum: 123}, 
+    {name: "umbreon", height: 1.0, type: "dark", nationalNum: 197},
+    {name: "machop", height: 0.8, type: "fighting", nationalNum: 180},
+    {name: "chimchar", height: 0.5, type: "fire", nationalNum: 390},
+    {name: "cubone", height: 0.4, type: "ground", nationalNum: 104},
+    {name: "infernape", height: 1.2, type: ["fire", "fighting"], nationalNum: 392},
+    {name: "ammonguss", height: 0.6, type: ["grass", "poison"], nationalNum: 591},
+    {name: "mewtwo", height: 2.0, type: "psychic", nationalNum: 150},
+    {name: "vaporeon", height: 1.0, type: "water", nationalNum: 134}
+];
