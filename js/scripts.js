d6870fc47c88315e9188bd58e41beb2856d71f86
Yemi Idowu <yemsonidowu@gmail.com>
2023-09-19 11:54:44 -0700

    Header and Iteration Loop
    Added a h1 tag to index.html to give more context on what is on the page. Made the pokemon list a little cleaner and organized, as well as made a for loop to iterate through the pokemon to get their names, and height.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,66 @@
 let pokemonList = [
-    {name: "abra", height: 0.9, type: "psychic", nationalNum: 63}, 
-    {name: "scyther", height: 1.5, type: ["bug", "flying"], nationalNum: 123}, 
-    {name: "umbreon", height: 1.0, type: "dark", nationalNum: 197},
-    {name: "machop", height: 0.8, type: "fighting", nationalNum: 180},
-    {name: "chimchar", height: 0.5, type: "fire", nationalNum: 390},
-    {name: "cubone", height: 0.4, type: "ground", nationalNum: 104},
-    {name: "infernape", height: 1.2, type: ["fire", "fighting"], nationalNum: 392},
-    {name: "ammonguss", height: 0.6, type: ["grass", "poison"], nationalNum: 591},
-    {name: "mewtwo", height: 2.0, type: "psychic", nationalNum: 150},
-    {name: "vaporeon", height: 1.0, type: "water", nationalNum: 134}
+    {
+        name: "Abra",
+        height: 0.9,
+        type: "psychic",
+        nationalNum: 63
+    }, 
+    {
+        name: "Scyther", 
+        height: 1.5, 
+        type: ["bug", "flying"], 
+        nationalNum: 123}, 
+    {
+        name: "Umbreon", 
+        height: 1.0, 
+        ype: "dark", 
+        nationalNum: 197},
+    {
+        name: "Machop", 
+        height: 0.8, 
+        type: "fighting", 
+        nationalNum: 180},
+    {
+        name: "Chimchar", 
+        height: 0.5, 
+        type: "fire", 
+        nationalNum: 390},
+    {
+        name: "Cubone", 
+        height: 0.4, 
+        type: "ground", 
+        nationalNum: 104},
+    {
+        name: "Infernape", 
+        height: 1.2, 
+        type: ["fire", "fighting"], 
+        nationalNum: 392},
+    {
+        name: "Gyarados", 
+        height: 21.3, 
+        type: ["water", "flying"], 
+        nationalNum: 130},
+    {
+        name: "Mewtwo", 
+        height: 2.0, 
+        type: "psychic", 
+        nationalNum: 150},
+    {
+        name: "Vaporeon",
+        height: 1.0, 
+        type: "water", 
+        nationalNum: 134
+    }
 ];
+
+// for loop to iterate through pokemon in pokemonList
+
+for (let i = 0; i <pokemonList.length; i++) {
+    if (pokemonList[i].height > 15) {
+        document.write(pokemonList[i].name + ` Height: ${pokemonList[i].height} - Wow that's big!`);
+        document.write("<br>");
+    } else {
+        document.write(pokemonList[i].name + ` Height: ${pokemonList[i].height} `);
+        document.write("<br>"); 
+    }
+}
