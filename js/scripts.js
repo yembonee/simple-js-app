2e3fc48e0aaae95681f09c68be8e3dd976355bca
Yemi Idowu <yemsonidowu@gmail.com>
2023-09-22 12:03:24 -0700

    Changed official layout and wrapping
    Wrapped the pokemonList array in IIFE within the pokemonRepository function. Added the add() and getAll() function within the head function. Kept the iteration of the forEach() function but just made sure it was calling through pokemonRepository because pokemonList was no longer a global object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,89 @@
-let pokemonList = [
-    {
-        name: "Abra",
-        height: 0.9,
-        type: "psychic",
-        nationalNum: 63
-    }, 
-    {
-        name: "Scyther", 
-        height: 1.5, 
-        type: ["bug", "flying"], 
-        nationalNum: 123}, 
-    {
-        name: "Umbreon", 
-        height: 1.0, 
-        ype: "dark", 
-        nationalNum: 197},
-    {
-        name: "Machop", 
-        height: 0.8, 
-        type: "fighting", 
-        nationalNum: 180},
-    {
-        name: "Chimchar", 
-        height: 0.5, 
-        type: "fire", 
-        nationalNum: 390},
-    {
-        name: "Cubone", 
-        height: 0.4, 
-        type: "ground", 
-        nationalNum: 104},
-    {
-        name: "Infernape", 
-        height: 1.2, 
-        type: ["fire", "fighting"], 
-        nationalNum: 392},
-    {
-        name: "Gyarados", 
-        height: 21.3, 
-        type: ["water", "flying"], 
-        nationalNum: 130},
-    {
-        name: "Mewtwo", 
-        height: 2.0, 
-        type: "psychic", 
-        nationalNum: 150},
-    {
-        name: "Vaporeon",
-        height: 1.0, 
-        type: "water", 
-        nationalNum: 134
+let pokemonRepository = ( function() {
+    let pokemonList = [
+        {
+            name: "Abra",
+            height: 0.9,
+            type: "psychic",
+            nationalNum: 63
+        }, 
+        {
+            name: "Scyther", 
+            height: 1.5, 
+            type: ["bug", "flying"], 
+            nationalNum: 123}, 
+        {
+            name: "Umbreon", 
+            height: 1.0, 
+            ype: "dark", 
+            nationalNum: 197},
+        {
+            name: "Machop", 
+            height: 0.8, 
+            type: "fighting", 
+            nationalNum: 180},
+        {
+            name: "Chimchar", 
+            height: 0.5, 
+            type: "fire", 
+            nationalNum: 390},
+        {
+            name: "Cubone", 
+            height: 0.4, 
+            type: "ground", 
+            nationalNum: 104},
+        {
+            name: "Infernape", 
+            height: 1.2, 
+            type: ["fire", "fighting"], 
+            nationalNum: 392},
+        {
+            name: "Gyarados", 
+            height: 21.3, 
+            type: ["water", "flying"], 
+            nationalNum: 130},
+        {
+            name: "Mewtwo", 
+            height: 2.0, 
+            type: "psychic", 
+            nationalNum: 150},
+        {
+            name: "Vaporeon",
+            height: 1.0, 
+            type: "water", 
+            nationalNum: 134
+        }
+    ]
+    // allows the ability to add/push a pokemon into the poemonList array
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-];
+    // will return the pokemonList array
+    function getAll() {
+        return pokemonList;
+    }
 
-// for loop to iterate through pokemon in pokemonList
+    return {
+        add: add,
+        getAll: getAll
+    }
+})();
 
-// for (let i = 0; i <pokemonList.length; i++) {
-//     if (pokemonList[i].height > 15) {
-//         document.write(pokemonList[i].name + ` Height: ${pokemonList[i].height} - Wow that's big!`);
-//         document.write("<br>");
-//     } else {
-//         document.write(pokemonList[i].name + ` Height: ${pokemonList[i].height} `);
-//         document.write("<br>"); 
-//     }
-// }
+// Makings sure the function above works, by calling it inside of console.
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name: "Raichu", height: 0.8, type: "Electric", nationalNum: 26});
+console.log(pokemonRepository.getAll());
 
 // forEach loop to iterate through pokemon in pokemonList
 
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon){ 
         if (pokemon.height > 15) {
+            // will write pokemon's name and height, special message if big enough.
         document.write(pokemon.name + ` Height: ${pokemon.height} - Wow that's big!`);
         document.write("<br>");
         } else {
         document.write(pokemon.name + ` Height: ${pokemon.height} `);
         document.write("<br>"); 
         }
-    }   
-)
+    }
+);
