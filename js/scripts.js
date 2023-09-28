cf345417e15c8c19a57552771ff080fc61b85a31
Yemi Idowu <yemsonidowu@gmail.com>
2023-09-28 13:26:56 -0700

    Comment update
    added some comments to better analyze what is on the js page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 let pokemonRepository = ( function() {
+
+    // Empty array to fill with the pokemon
+
     let pokemonList = [];
+
+    // URL source for the pokemon and their attributes
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=1292';
+
     // allows the ability to add/push a pokemon into the poemonList array
     function add(pokemon) {
         if (
@@ -13,11 +19,13 @@ let pokemonRepository = ( function() {
             console.log("pokemon is not correct")
         }
     }
+
     // will return the pokemonList array
     function getAll() {
         return pokemonList;
     }
 
+    //function to add a pokemon to the list
     function addListItem(pokemon) {
         let ulPokemon = document.querySelector(".pokemon-list");
 
@@ -37,6 +45,7 @@ let pokemonRepository = ( function() {
         });
     }
 
+    // function that loads the object in the console
     function loadList() {
         return fetch(apiUrl).then(function (response) {
             return response.json();
@@ -53,7 +62,8 @@ let pokemonRepository = ( function() {
             console.error(e);
         })
     }
-
+    
+    // function that allows the showModal() function to be called
     function loadDetails(item) {
         let url = item.detailsUrl;
         return fetch(url).then(function (response) {
@@ -68,38 +78,47 @@ let pokemonRepository = ( function() {
         });
     }
 
+    // calls the loadDetails function onto the site.
     function showDetails(pokemon) {
         loadDetails(pokemon);
     };
 
+    // function that creates a modal of the pokemon's picture, name, and other attributes when that pokemon's button is pressed.
     function showModal(pokemon) {
         let modalContainer = document.querySelector('#modal-container');
     
         modalContainer.innerHTML = '';
     
+        // making a div element with class
         let modal = document.createElement('div');
         modal.classList.add('modal');
     
+        //making button element with class and event listener
         let closeButtonElement = document.createElement('button');
         closeButtonElement.classList.add('modal-close');
         closeButtonElement.innerText = 'CLOSE';
         closeButtonElement.addEventListener('click', hideModal)
     
+        //making h1 tag for pokemon name
         let titleElement = document.createElement('h1');
         titleElement.innerText = pokemon.name;
     
+        //making img element to display the src of the pokemon
         let imgElement = document.createElement('img');
         imgElement.src = pokemon.imageUrl;
 
+        //making p element which shows the content of pokemon
         let contentElement = document.createElement('p');
         contentElement.innerText = 'Height: ' + pokemon.height;
     
+        //adding the button, img, h1, and p element to the main div element
         modal.appendChild(closeButtonElement);
         modal.appendChild(imgElement);
         modal.appendChild(titleElement);
         modal.appendChild(contentElement);
         modalContainer.appendChild(modal);
       
+        //adding css attribute for the modal so when it is clicked, it becomes visible
         modalContainer.classList.add('is-visible');
     
         modalContainer.addEventListener('click', (e) => {
@@ -112,7 +131,8 @@ let pokemonRepository = ( function() {
       }
     
       let dialogPromiseReject;
-      
+
+      //function to close the modal once opened
       function hideModal() {
         let modalContainer = document.querySelector('#modal-container');
         modalContainer.classList.remove('is-visible');
